9ccf52edb053d4a5fc66ee6ff6923e64cf829349
roys-esh <schonberg.r@gmail.com>
2026-04-20 11:31:26 +0300

    Remove unused Text import and component from App.tsx

--- App.tsx
+++ App.tsx
@@ -6,12 +6,11 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-import { Text } from 'react-native/types_generated/index';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -20,7 +19,6 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
-      <Text>App.tsx</Text>
     </SafeAreaProvider>
   );
 }
